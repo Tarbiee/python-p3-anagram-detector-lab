3f38a4702d9f81789ef3ac830dbb7d0e4272b7e8
Tarbiee <dorcusstaby@gmail.com>
2023-11-29 17:00:50 +0300

    Complted Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self,word=""):
+        self.word = word
+
+    def match(self, words):
+        list = []
+        for char in words:
+            if sorted(self.word) == sorted(char):
+                list.append(char)
+        return list
